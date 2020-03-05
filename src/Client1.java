4fa6215724e41b7b95ad7b063fddfaa35ae66d5f
wanghao18 <wanghao18@staff.weibo.com>
2020-03-05 10:05:17 +0800

    first commit

---
+++ src/Client1.java
@@ -0,0 +1,9 @@
+public class Client1 {
+    public static void main(String[] args) {
+        UserService userServiceImpl = new UserServiceImpl();
+        UserService proxy = new UserServiceProxy(userServiceImpl);
+
+        proxy.select();
+        proxy.update();
+    }
+}
---
+++ src/UserService.java
@@ -0,0 +1,4 @@
+public interface UserService {
+    public void select();
+    public void update();
+}
